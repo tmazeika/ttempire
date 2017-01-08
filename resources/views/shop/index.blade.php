2df23cd0796fb6b9c3acfdc7042342868f8409bf
T.J. Mazeika <bionicrm@gmail.com>
2017-01-08 01:46:21 -0500

    Add shopping cart and glow

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -3,20 +3,26 @@
 @section('content')
     <section class="shop">
         @foreach($products as $product)
-            <div class="shop-product">
-                <img class="shop-product-img" src="{{ $product->getImg() }}"/>
-
-                <div class="shop-product-info">
-                    <h1 class="shop-product-desc">{{ $product->getDesc() }}</h1>
-                    <sub class="shop-product-title">{{ $product->getTitle() }}</sub>
+            <div class="shop-product-container">
+                <div class="shop-product-cart">
+                    <img src="{{ asset('img/fa_shopping_cart.svg') }}" alt="Add to cart" width="25"/>
                 </div>
 
-                <div class="spacer"></div>
+                <div class="shop-product" data-product-id="{{ $product->getId() }}">
+                    <img class="shop-product-img" src="{{ $product->getImg() }}"/>
 
-                <div class="shop-product-action">
-                    <div class="shop-product-action-item">${{ $product->getPrice() }}</div>
-                    <sub class="shop-product-action-item sub long" hidden>{{ $product->getQtyInc() }} per box</sub>
-                    <sub class="shop-product-action-item sub short" hidden>{{ $product->getQtyInc() }}/box</sub>
+                    <div class="shop-product-info">
+                        <h1 class="shop-product-desc">{{ $product->getDesc() }}</h1>
+                        <sub class="shop-product-title">{{ $product->getTitle() }}</sub>
+                    </div>
+
+                    <div class="spacer"></div>
+
+                    <div class="shop-product-action">
+                        <div class="shop-product-action-item">${{ $product->getPrice() }}</div>
+                        <sub class="shop-product-action-item sub long" hidden>{{ $product->getQtyInc() }} per box</sub>
+                        <sub class="shop-product-action-item sub short" hidden>{{ $product->getQtyInc() }}/box</sub>
+                    </div>
                 </div>
             </div>
         @endforeach
