2747128df9626a4d5547b77d04f09ecd11441b86
T.J. Mazeika <bionicrm@gmail.com>
2017-01-15 10:03:05 -0500

    Format helpers.php

--- app/helpers.php
+++ app/helpers.php
@@ -1,25 +1,16 @@
 <?php
 
-/**
- * Generate a querystring url for the application.
- *
- * Assumes that you want a URL with a querystring rather than route params
- * (which is what the default url() helper does)
- *
- * @param  string  $path
- * @param  mixed   $qs
- * @param  bool    $secure
- * @return string
- */
-function qs_url($path = null, $qs = array(), $secure = null)
+function qs_url($path = null, $qs = array(), $secure = null) : string
 {
     $url = app('url')->to($path, $secure);
-    if (count($qs)){
 
-        foreach($qs as $key => $value){
-            $qs[$key] = sprintf('%s=%s',$key, urlencode($value));
+    if (count($qs)) {
+        foreach($qs as $key => $value) {
+            $qs[$key] = sprintf('%s=%s', $key, urlencode($value));
         }
+
         $url = sprintf('%s?%s', $url, implode('&', $qs));
     }
+
     return $url;
 }
