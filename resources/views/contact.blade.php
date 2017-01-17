5f4207687ab8bb9c643f19bf82526c80b3388679
T.J. Mazeika <bionicrm@gmail.com>
2017-01-16 19:42:16 -0500

    Furnish home and contact pages, and fix mobile checkout buttons

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,9 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Contact</p>
-@endsection
-
-@section('scripts')
-    @include('partials.js.app')
+    <section class="contact">
+        <div class="contact-card">
+            <span class="contact-item-title">email</span><br/>
+            thepingpongshop&commat;gmail.com<br/>
+            <span class="contact-item-title">address</span><br/>
+            Lengdngsvdgen 5, 81275 Stocksund, Sweden<br/>
+            <span class="contact-item-title">phone</span><br/>
+            +46 765912848
+        </div>
+    </section>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>Home</p>
+    <section class="main-jumbo">
+        <div class="main-jumbo-container">
+            <div class="main-jumbo-welcome">
+                Welcome<br/>
+                <span class="main-jumbo-welcome-small">to the</span><br/>
+                Ping Pong Shop
+            </div>
+
+            <div class="main-jumbo-shop">
+                <a href="{{ url('/shop') }}">
+                    <button class="main-jumbo-shop-btn">
+                        Shop Now
+                    </button>
+                </a>
+            </div>
+        </div>
+    </section>
 @endsection
