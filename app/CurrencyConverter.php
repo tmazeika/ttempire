2759d4d7a10108ebd0f1ee1730f11a6e4b76e556
T.J. Mazeika <bionicrm@gmail.com>
2017-01-17 21:59:05 -0500

    Refactor CurrencyConverter

--- app/CurrencyConverter.php
+++ app/CurrencyConverter.php
@@ -13,10 +13,10 @@ class CurrencyConverter
         $rate = 1;
 
         if ($from !== $to) {
-            $rate = Cache::remember('yql.' . $from . '.' . $to, self::RATE_CACHE_MINUTES, function () use ($from, $to) {
+            $rate = Cache::remember('yql.'.$from.'.'.$to, self::RATE_CACHE_MINUTES, function() use ($from, $to) {
                 $yql_base_url = 'http://query.yahooapis.com/v1/public/yql';
-                $yql_query = "select * from yahoo.finance.xchange where pair in (\"$from$to\")";
-                $yql_query_url = $yql_base_url . '?q=' . urlencode($yql_query);
+                $yql_query = "SELECT * FROM yahoo.finance.xchange WHERE pair IN (\"$from$to\")";
+                $yql_query_url = $yql_base_url.'?q='.urlencode($yql_query);
                 $yql_query_url .= '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
                 $yql_session = file_get_contents($yql_query_url);
                 $yql_json = json_decode($yql_session, true);
@@ -25,6 +25,6 @@ class CurrencyConverter
             });
         }
 
-        return round($amount * $rate);
+        return round($amount * $rate, 5);
     }
 }
