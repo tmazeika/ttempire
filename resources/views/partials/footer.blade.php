7d416b7ccd0b6227342ce7f53b0dd6434551b867
T.J. Mazeika <bionicrm@gmail.com>
2017-01-19 20:35:33 -0500

    Add language drop down

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -6,6 +6,11 @@
     <div class="spacer"></div>
 
     <div class="footer-item">
+        <a class="footer-item-link {{ LaravelLocalization::getCurrentLocale() === 'en-US' ? 'active' : '' }}" href="{{ LaravelLocalization::getLocalizedURL('en-US') }}">USD</a>
+        <a class="footer-item-link {{ LaravelLocalization::getCurrentLocale() === 'en-GB' ? 'active' : '' }}" href="{{ LaravelLocalization::getLocalizedURL('en-GB') }}">EUR</a>
+    </div>
+
+    <div class="footer-item">
         built by <a href="https://mazeika.me">T.J. Mazeika</a>
     </div>
 </footer>
