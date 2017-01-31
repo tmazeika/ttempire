0445df24cedb48501ce1166f953ac57a1f5a0fe0
T.J. Mazeika <bionicrm@gmail.com>
2017-01-30 19:22:04 -0500

    Update contact information

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,11 +4,7 @@
     <section class="contact">
         <div class="contact-card">
             <span class="contact-item-title">email</span><br/>
-            thepingpongshop&commat;gmail.com<br/>
-            <span class="contact-item-title">address</span><br/>
-            Lengdngsvdgen 5, 81275 Stocksund, Sweden<br/>
-            <span class="contact-item-title">phone</span><br/>
-            +46 765912848
+            support&commat;ttempire.com
         </div>
     </section>
 @endsection
