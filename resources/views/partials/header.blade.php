76dbd3a991ea9dbbbbd4615e3c64ec6752dfd070
T.J. Mazeika <mazeika.tj@gmail.com>
2017-04-10 23:35:03 -0400

    Create basic header and style

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header class="header">
+    <nav class="header-nav">
+        <a class="header-nav-item" href="{{ url('/') }}">
+            <img class="header-nav-item-img" src="{{ asset('img/logo.png') }}"/>
+        </a>
+        <a class="header-nav-item" href="{{ url('shop') }}">Shop</a>
+        <a class="header-nav-item" href="{{ url('contact') }}">Contact</a>
+    </nav>
+</header>
