178f32262c5a6341c445819f0d548e7e684619c3
T.J. Mazeika <mazeika.tj@gmail.com>
2017-04-10 23:45:18 -0400

    Add footer and style

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+    <span class="footer-item">&copy; {{ date('Y') }} Table Tennis Empire</span>
+    <div class="flex-spacer"></div>
+    <span class="footer-item">Built by <a href="https://mazeika.me">T.J. Mazeika</a></span>
+</footer>
