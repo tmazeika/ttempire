7c1ae70180870900b5823fb658bd4564d135b99a
T.J. Mazeika <mazeika.tj@gmail.com>
2017-04-15 17:22:53 -0400

    Use Google site verification

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="7lJH_awwD78EYulxxv-rakpDEyrlUJbO5mgAr937V68">
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
