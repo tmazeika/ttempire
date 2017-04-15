2fd2cc48ea06d5dabc248b0086dd725662b02c99
T.J. Mazeika <mazeika.tj@gmail.com>
2017-04-15 12:25:35 -0400

    Add OnPull command

---
+++ app/Console/Commands/OnPull.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace TTEmpire\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class OnPull extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'on-pull';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Does some maintenance on Git pull';
+
+    /**
+     * Create a new command instance.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('migrate');
+
+        $cwd = getcwd();
+        chdir(base_path());
+
+        // composer update
+        $root = base_path();
+        `composer -d $root -n update`;
+
+        // npm update
+        `npm update`;
+
+        // npm run script
+        $script = env('APP_ENV', 'production');
+
+        if ($script !== 'production') {
+            $script = 'dev';
+        }
+
+        `npm run $script`;
+
+        chdir($cwd);
+    }
+}
